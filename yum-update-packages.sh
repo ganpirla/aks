739918e225a2cb067b560c43e27c70a6cee73748
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2023-03-30 00:20:21 +0530

    Update yum-update-packages.sh

--- yum-update-packages.sh
+++ yum-update-packages.sh
@@ -1,5 +1,3 @@
-yum-update-packages.sh
-
 #!/bin/bash
 
 # Run the "yum check-update" command and store the output in a variable
@@ -10,10 +8,11 @@ while read -r line; do
   # Extract the package name and version from each line
   package_name=$(echo "$line" | awk '{print $1}')
   package_version=$(echo "$line" | awk '{print $2}')
+  package_release=$(echo "$line" | awk '{print $3}')
 
   # Replace any periods in the version with hyphens
   package_version=${package_version//./-}
 
-  # Print the installable package name
-  echo "${package_name}-${package_version}"
+  # Print the installable package name in RPM format
+  echo "${package_name}-${package_version}-${package_release}"
 done <<< "$yum_output"
