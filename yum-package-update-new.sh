91de8e2bcb0bcdaa7a836069cc4b8a2799a0201c
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2023-03-30 11:13:17 +0530

    Update yum-package-update-new.sh

--- yum-package-update-new.sh
+++ yum-package-update-new.sh
@@ -3,15 +3,8 @@
 # Run the "yum -q check-update" command and capture the output
 output=$(yum -q check-update)
 
-# Extract the package name, version and architecture from each line of the output
-while read -r line; do
-    package=$(echo "$line" | awk '{print $1}')
-    version=$(echo "$line" | awk '{print $2}')
-    arch=$(echo "$line" | awk '{print $3}')
-    
-    # Replace the dot in the package name with a hyphen
-    formatted_package=$(echo "$package" | sed 's/\./-/g')
-    
-    # Print the package name, version and architecture in the desired format
-    printf "%s.%s\t%s.%s\n" "$formatted_package" "$arch" "$version" "$arch"
-done <<< "$output"
+# Filter the output to remove lines containing only "=" characters
+filtered_output=$(echo "$output" | grep -v '^=\+$')
+
+# Print the modified output
+echo "$filtered_output"
