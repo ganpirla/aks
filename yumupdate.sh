6f1c28dde03c77f4ab9adf07648e8ed15bf82e57
Pirla Gangadhara Rao <60540333+ganpirla@users.noreply.github.com>
2023-04-04 08:58:00 +0530

    Delete yumupdate.sh

--- yumupdate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run the yum update command and store the output in a variable
-yum_update_output=$(yum update 2>&1)
-
-# Extract the package names using awk and print them
-echo "$yum_update_output" | awk '/^([a-zA-Z0-9_-]+\.){2}[a-zA-Z0-9_-]+/{print $1}'
